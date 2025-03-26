baa538a021532befa6901b3c453448e1f3ecd57d
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-03-26 17:12:01 -0300

    feat: Showing account balance using the old fashioned way

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,24 @@
+// import { useSelector } from "react-redux";
+
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
+	return new Intl.NumberFormat("en", {
+		style: "currency",
+		currency: "USD",
+	}).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+	// const balance = useSelector((store) => store.account.balance);
+
+	return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(store) {
+	return {
+		balance: store.account.balance,
+	};
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
